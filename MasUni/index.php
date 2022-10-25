040c55fb5737a5b751bf8ea145b61d918990d904
m-peeler <111776404+m-peeler@users.noreply.github.com>
2022-10-25 14:25:33 -0400

    Current Website Files

---
+++ MasUni/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+
+<head>
+    <h1 style = "text-align:left">
+        MasUni
+    </h1>
+
+    <h2 style = "font-weight: normal">
+        Welcome To MasUni
+    </h2>
+</head>
+
+<body>
+    
+</body>
+<body>
+   <h3 style = "font-weight: normal">
+    Our Goal:
+   </h3>
+   <p style = "text-align:left" style = "font-weight:bold"> Connect companies with gigs <br>
+   to prospective students across the country</p>
+   
+
+</body>
+
+
+
+<body>
+<h3>Quick Links</h3>
+<ul>
+        <li> 
+        <a href = "">
+            Home Page
+        </a>
+        </li>
+
+        <li>
+        <a href = "./login">
+            Login
+        </a>
+    </ul>
+</body>
+
+
+</html>
---
+++ MasUni/login/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Welcome To MasUni</title>
+  <link rel="stylesheet" href="login-page.css">
+  <script defer src="script.js"></script>
+</head>
+
+<body>
+  <main id="main-holder">
+    <h1 id="login-header">MasUni Login</h1>
+    
+    <div id="login-error-msg-holder">
+      <p id="login-error-msg">Invalid username <span id="error-msg-second-line">and/or password</span></p>
+    </div>
+    <form action="#" method="post" id="login-form">
+
+      <section>      
+	<label for="username">Username</label>  
+        <input id="username" name="username" type="text" placeholder="Username" autocomplete="username" required>
+      </section>
+            
+      <section>        
+        <label for="current-password">Password</label>
+        <input id="current-password" name="current-password" type="password" placeholder="Password" autocomplete="current-password" aria-describedby="password-constraints" required>
+        <button id="toggle-password" type="button" aria-label="Show password as plain text. Warning: this will display your password on the screen.">Show password</button>
+        <div id="password-constraints">Eight or more characters, with at least one&nbsp;lowercase and one uppercase letter.</div>
+      </section>
+
+      <button id="signin">Sign in</button>
+      
+    </form>
+  
+  </main>
+</body>
+
+</html>
