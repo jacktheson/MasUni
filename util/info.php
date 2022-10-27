e4f4310683e11d25c3982811bd4b173e395f3ef2
Jackson Durst <jtdurst01@gmail.com>
2022-10-27 14:37:17 -0400

    added password hash and fixed file path and sql dattime

--- util/info.php
+++ util/info.php
@@ -6,6 +6,15 @@ $dbname = "MasUni";
 $con = new mysqli($server,$username,$password,$dbname);
 unset($username,$password);
 if(mysqli_connect_errno()){
-    echo "Failed to connect to server: " . mysqli_connect_errno;
+    echo "Failed to connect to server: " . mysqli_connect_errno();
+}
+
+function hashPassword($password,$salt){
+    $pepper = 1020002808575613;
+    $hash = hash("sha3-256",$password);
+    $hash = hash("sha3-256", $hash + $salt);
+    $hash = hash("sha3-256", $hash + $pepper);
+
+    return $hash;
 }
 ?>
