a3ac392e96f81b5559f534f5877563fccc60fd33
phillipgarcia12 <suppliedsum12@gmail.com>
2022-10-27 14:35:25 -0400

    changed mysqlit to ()

--- util/info.php
+++ util/info.php
@@ -6,6 +6,6 @@ $dbname = "MasUni";
 $con = new mysqli($server,$username,$password,$dbname);
 unset($username,$password);
 if(mysqli_connect_errno()){
-    echo "Failed to connect to server: " . mysqli_connect_errno;
+    echo "Failed to connect to server: " . mysqli_connect_errno();
 }
 ?>
