da178d620cde461e71e746bd09ade0265a518bfb
Jackson Durst <jtdurst01@gmail.com>
2022-10-27 13:43:48 -0400

    added sql connect in info

--- util/info.php
+++ util/info.php
@@ -1,6 +1,11 @@
 <?php
-$server = localhost;
-$username = jdurst;
-$password =  MasUni123;
-$dbname = MasUni;
+$server = "localhost";
+$username = "jdurst";
+$password =  "MasUni123";
+$dbname = "MasUni";
+$con = new mysqliconnect($server,$username,$password,$dbname);
+unset($username,$password);
+if(mysqli_connect_errno()){
+    echo "Failed to connect to server: " . mysqli_connect_errno;
+}
 ?>
