8c23586557106978dca424cd95cef6f8db8d50c9
Tate Rosen <84721398+taterosen@users.noreply.github.com>
2022-10-30 20:34:38 -0400

    logout page
    file that shows when user has logged out

---
+++ MasUni/logout/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Logout Page</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <div class="form">
+        <p>You have logged out successfully.<br>
+		Thank you for choosing MasUni!<br><br></p>
+
+	<p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
+    </div>
+</body>
+</html>
