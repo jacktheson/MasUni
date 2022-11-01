76c9c8fe9eb85dbc6011b0ff0ad945ceb5180179
Tate Rosen <84721398+taterosen@users.noreply.github.com>
2022-11-01 14:05:37 -0400

    changed logout path
    updated logout path so it goes to logout page

--- MasUni/dashboard/index.php
+++ MasUni/dashboard/index.php
@@ -13,7 +13,7 @@ include("auth_session.php");
     <div class="form">
         <p>Hey, <?php echo $_SESSION['username']; ?>!</p>
         <p>You are now user dashboard page.</p>
-        <p><a href="logout.php">Logout</a></p>
+        <p><a href="../logout">Logout</a></p>
     </div>
 </body>
 </html>
