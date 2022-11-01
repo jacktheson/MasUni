ebac9314638a4ba39aea19896568b12089d108fb
m-peeler <111776404+m-peeler@users.noreply.github.com>
2022-11-01 16:11:09 -0400

    Updated login

--- util/info.php
+++ util/info.php
@@ -1,24 +1,81 @@
 <?php
-$server = "localhost";
-$username = "jdurst";
-$password =  "MasUni123";
-$dbname = "MasUni";
-$con = new mysqli($server,$username,$password,$dbname);
-unset($username,$password);
-if(mysqli_connect_errno()){
-    echo "Failed to connect to server: " . mysqli_connect_errno();
+require("session_create.php");
+
+function hashPasswordForUser($password, $username) {
+    return hashPassword($password, getSaltFor($username));
 }
 
-function hashPassword($password,$salt){
-    $pepper = 1020002808575613;
-    $hash = hash("sha-256",$password);
-    $hash = hash("sha-256", $hash + $salt);
-    $hash = hash("sha-256", $hash + $pepper);
-
+function hashPassword($password, $salt){
+    $pepper = 102002808575613;
+    $hash = md5($password . $salt . $pepper);
+    unset($pepper, $password, $salt);
     return $hash;
 }
 
 function generateSalt(){
-    return rand(0,2147483647);
+    return rand(0, 2147483647);
+}
+
+function getSaltFor($username){
+    $con = sql_connection();
+    $saltQuery = "SELECT `salt` FROM `USER_LOGIN` WHERE username='$username'";
+    $result = mysqli_query($con, $saltQuery) or die("mySQL query salt failed");
+    $salt = $result->fetch_assoc()['salt'];
+    unset($result, $saltQuery);
+    return $salt;
+}
+
+function cleanUserInput($val){
+    $val = stripslashes($val);
+    $con = sql_connection();
+    $rtrn = $con->real_escape_string($val);
+    $con->close();
+    return $rtrn;
+}
+
+function queryDatabase($query){ 
+    $con = sql_connection();
+    $res = $con->query($query);
+    if(!queryResponse) die('Error: ' . mysqli_error($con));
+    $con->close();
+    return $res;
+}
+
+function querySucceeded($queryResponse) {
+    die(!$queryResponse . " | " . mysqli_num_rows($queryResponse) . " | ");
+    if(!$queryResponse) return FALSE;
+    if(mysqli_num_rows($queryResponse) > 0) return TRUE;
+    return FALSE;
+}
+
+function createAccount($username, $email, $password) {
+        // removes backslashes
+        $username = cleanUserInput($username);
+        $email    = cleanUserInput($password);
+        $password = cleanUserInput($email);
+	$salt = generateSalt();
+        $hashedPassword = hashPassword($password, $salt);
+        $query    = "INSERT into `USER_LOGIN` (username, hashedPassword, email, salt)
+                     VALUES ('$username', '$hashedPassword', '$email' , '$salt')";
+        $result   = queryDatabase($query);
+	return $result != FALSE;
+}
+
+function loginCorrect($username, $password){
+    $username = cleanUserInput($username);
+    $password = cleanUserInput($password);
+    $hashedPassword = hashPasswordForUser($password, $username);
+    die($hashedPassword);
+    $query = "SELECT * FROM `USER_LOGIN` WHERE username='$username' AND hashedPassword='$hashedPassword'"; 
+    $con = sql_connection();
+    $loginSuccess = mysqli_query($con, $query) or die("mySQL query failed");
+    $con->close();
+    if (mysqli_num_rows($loginSuccess) > 0){
+        die( $username . " | " . mysqli_num_rows($loginSuccess) . " | " . $loginSuccess->fetch_assoc()["hashedPassword"] . " | HHH");
+        return TRUE;
+    } else {
+        die($username . " | " . $loginSuccess->fetch_assoc()["hashedPassword"]);
+        return FALSE;
+    }
 }
 ?>
