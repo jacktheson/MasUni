170daed77b48d669544152bcb53e1a52ea223458
m-peeler <mdavispeeler@gmail.com>
2022-11-03 15:09:41 -0400

    Updated student query check

--- MasUni/students/index.php
+++ MasUni/students/index.php
@@ -2,7 +2,7 @@
 include '../../../util/info.php';
 $query = "SELECT first_name, last_name, university, graduation_year, primary_major FROM USER_DATA";
 $result = queryDatabase($query); 
-if(querySucceeded($result))
+if($result->num_rows > 0)
 {
     while ($row = $result -> fetch_assoc())
     {
