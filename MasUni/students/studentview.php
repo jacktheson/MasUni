942b3422d5e28497f799c944168f6752084af7e4
Phillip Garcia <113802786+phillipgarcia12@users.noreply.github.com>
2022-11-03 13:29:32 -0400

    added in student view and HTML table

---
+++ MasUni/students/studentview.php
@@ -0,0 +1,15 @@
+<?php
+include 'info.php';
+$query = "SELECT first_name, last_name, university, graduation,   FROM USER_DATA";
+$result = queryDatabase($query); 
+if($result -> num_rows > 0)
+{
+    while ($row = $result -> fetch_assoc())
+    {
+        echo "First Name: ". $row["first_name"]. "Last Name; ". $row["last_name"]. "University; ". $row["univesrity"]. "Graduation Year: ". $row["graduation_year"]. "Major". $row["[primary_major"];
+    }
+}
+echo "0 results found";
+
+$result->close();
+?>
